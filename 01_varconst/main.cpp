fc7480837f83cd1eb6022ff0ab46f7c5d900de07
Bedant Chhetri <bedantchhetri7@gmail.com>
2025-04-23 08:49:25 +0530

    variable and constants in c++

---
+++ 01_varconst/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int score;// declare a variable
+    score = 100;// assign a value to the variable (initialization)
+    int balanece = 1000;// declare and initialize a variable
+
+    const int uid = 1001;// declare a constant variable
+    cout<<"welcome to chai with cpp"<<endl;
+    return 0;
+}
