6df81e2a6c1fefee19e2700507426539d2aee11f
Bedant Chhetri <bedantchhetri7@gmail.com>
2025-04-27 20:47:25 +0530

    primitive data types

---
+++ 02_primitives/data.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int teaLeaves = 50;
+    float waterTemp = 85.5;
+    double priceOfTea = 20.98;
+    char teaGrade = 'A';
+    bool isReady = true;
+    return 0;
+}
