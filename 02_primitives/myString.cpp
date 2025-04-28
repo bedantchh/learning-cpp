129799e0b4c07772e6b844e40149145152e806e8
Bedant Chhetri <bedantchhetri7@gmail.com>
2025-04-28 16:32:41 +0530

    strings

---
+++ 02_primitives/myString.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string favTea = "Lemon Tea \n";
+    string description = "Known as \"best\" tea";
+    cout << favTea << endl << description;
+    return 0;
+}
